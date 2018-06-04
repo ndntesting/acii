86daea8cc5e54a84babf39ebe29ccf8012aba13a
ndntesting <ndntesting@outlook.com>
2018-06-04 22:10:48 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/acii/*.csproj && dotnet build src/acii/*.csproj
---
+++ src/acii/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace acii
+{
+    public class Class1
+    {
+    }
+}
